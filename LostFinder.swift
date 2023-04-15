707693fc826fb3293a8eb4e67a9ff9f83c5cecaf
Xurmatillo <96421607+Xurmatillo@users.noreply.github.com>
2023-04-15 18:37:02 +0500

    Update LostFinder.swift

--- LostFinder.swift
+++ LostFinder.swift
@@ -1,46 +1,87 @@
 import UIKit
+import AVFoundation
 
-class HomeViewController: UIViewController {
+class ViewController: UIViewController, AVCapturePhotoCaptureDelegate {
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // set up the UI elements for the home screen
-    let lostItemsButton = UIButton()
-    lostItemsButton.setTitle("Find Lost Items", for: .normal)
-    lostItemsButton.addTarget(self, action: #selector(findLostItems), for: .touchUpInside)
-    view.addSubview(lostItemsButton)
+    // UI Outlets
+    @IBOutlet weak var itemNameTextField: UITextField!
+    @IBOutlet weak var cameraView: UIView!
+    @IBOutlet weak var resultsLabel: UILabel!
 
-    let recognizeTextButton = UIButton()
-    recognizeTextButton.setTitle("Recognize Texts", for: .normal)
-    recognizeTextButton.addTarget(self, action: #selector(recognizeTexts), for: .touchUpInside)
-    view.addSubview(recognizeTextButton)
+    // AVCaptureSession variables
+    var captureSession: AVCaptureSession!
+    var photoOutput: AVCapturePhotoOutput!
+    var previewLayer: AVCaptureVideoPreviewLayer!
 
-    let identifyObjectsButton = UIButton()
-    identifyObjectsButton.setTitle("Identify Objects", for: .normal)
-    identifyObjectsButton.addTarget(self, action: #selector(identifyObjects), for: .touchUpInside)
-    view.addSubview(identifyObjectsButton)
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
 
-    // layout the UI elements for the home screen
-    lostItemsButton.translatesAutoresizingMaskIntoConstraints = false
-    lostItemsButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-    lostItemsButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        // Setup capture session
+        captureSession = AVCaptureSession()
+        captureSession.sessionPreset = .photo
 
-    recognizeTextButton.translatesAutoresizingMaskIntoConstraints = false
-    recognizeTextButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-    recognizeTextButton.topAnchor.constraint(equalTo: lostItemsButton.bottomAnchor, constant: 16).isActive = true
+        // Get input device
+        let captureDevice = AVCaptureDevice.default(for: .video)
 
-    identifyObjectsButton.translatesAutoresizingMaskIntoConstraints = false
-    identifyObjectsButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-    identifyObjectsButton.topAnchor.constraint(equalTo: recognizeTextButton.bottomAnchor, constant: 16).isActive = true
-  }
+        do {
+            // Add input device to session
+            let input = try AVCaptureDeviceInput(device: captureDevice!)
+            captureSession.addInput(input)
 
-  @objc func findLostItems() {
-    // navigate to the screen for finding lost items
-  }
+            // Setup photo output
+            photoOutput = AVCapturePhotoOutput()
+            photoOutput.isHighResolutionCaptureEnabled = true
+            captureSession.addOutput(photoOutput)
 
-  @objc func recognizeTexts() {
-    // navigate to the screen for recognizing texts
-  }
+            // Setup preview layer
+            previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+            previewLayer.videoGravity = .resizeAspectFill
+            previewLayer.frame = cameraView.bounds
+            cameraView.layer.addSublayer(previewLayer)
 
-  @objc func identifyObjects() {
-    // navigate to the screen for
+            // Start session
+            captureSession.startRunning()
+
+        } catch {
+            print(error)
+        }
+    }
+
+    // Function to capture photo when button is pressed
+    @IBAction func scanButtonPressed(_ sender: UIButton) {
+        let settings = AVCapturePhotoSettings()
+        photoOutput.capturePhoto(with: settings, delegate: self)
+    }
+
+    // Delegate function to handle captured photo
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        // Convert captured photo to UIImage
+        guard let imageData = photo.fileDataRepresentation(), let image = UIImage(data: imageData) else { return }
+
+        // Analyze image using machine learning model
+        let lostItem = analyzeImage(image: image)
+
+        // Display results to user
+        displayResults(itemName: lostItem)
+    }
+
+    // Function to analyze image and identify lost item
+    func analyzeImage(image: UIImage) -> String? {
+        // Use Core ML model to analyze image and identify lost item
+        // Return name of lost item, or nil if no item is found
+        return nil
+    }
+
+    // Function to display results to user
+    func displayResults(itemName: String?) {
+        if let itemName = itemName {
+            // Display name of lost item
+            resultsLabel.text = "Found: \(itemName)"
+        } else {
+            // Display message if no item is found
+            resultsLabel.text = "No items found"
+        }
+    }
+
+}
